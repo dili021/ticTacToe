d374ea2b94044bce1ffaba3b242cdc3c3e4f2e68
stefan dili <stefan.dili.021@gmail.com>
2020-08-28 14:08:11 +0200

    game logic done

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-import { playerFactory } from './factories.js';
-// import { checkForWinner, checkForDraw } from './utils.js';
-
-// TODO const gameLogic = (() => {})();
-
-const board = () => {
+import * as elements from './DOMelements.js';
+import playerFactory from './factories.js';
+import { inputIsValid } from './utils.js';
+export const board = () => {
   const field = [null, null, null, null, null, null, null, null, null];
-  const cells = [...document.querySelectorAll('.cell')];
+  const winLines = [
+    // horizontal
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const getField = () => field;
+
+  const getWinLines = () => winLines;
 
   const writeToField = (idx, mark) => {
     field[idx] = mark;
@@ -13,33 +27,64 @@ const board = () => {
 
   const render = () => {
     for (let i = 0; i < field.length; i++) {
-      cells[i].textContent = field[i];
+      elements.cells[i].textContent = field[i];
     }
   };
 
-  return { render, writeToField };
+  return { render, writeToField, getField, getWinLines };
 };
 
 const game = ((gameBoard, p1 = 'Player One', p2 = 'Player Two') => {
   const board = gameBoard();
   const player1 = playerFactory(p1, 'X');
   const player2 = playerFactory(p2, 'O');
+
   let currentPlayer = player1;
 
-  (() => {
-    board.render();
-  })();
+  (() => board.render())();
 
   const changePlayer = () => {
     currentPlayer = currentPlayer === player1 ? player2 : player1;
   };
+  // check for end game
+  const hasGameEnded = () => {
+    console.log(playerHasWon(currentPlayer));
+    if (playerHasWon(currentPlayer) || gameIsDraw()) endGame();
+  };
+
+  const showEndModal = () => {
+    elements.endModal.style.display = 'grid';
+  };
+
+  const endGame = () => {
+    showEndModal() && grid.removeEventListener('click', game.move);
+  };
+
+  const playerHasWon = player => {
+    board.getWinLines().forEach(line => {
+      if (line.every(spot => board.getField()[spot] === player.mark)) {
+        endGame();
+      }
+    });
+  };
+
+  const gameIsDraw = () => {
+    if (
+      board.getField().every(spot => spot !== null) &&
+      !playerHasWon(currentPlayer)
+    ) {
+      console.log("It's a draw!");
+    }
+  };
+
+  const validateMove = () => {};
 
   const move = e => {
-    const { mark } = currentPlayer;
     const index = e.target.dataset.cellIdx;
+    const { mark } = currentPlayer;
+    if (!inputIsValid(board, index)) move();
     board.writeToField(index, mark);
-    // checkForWinner(board);
-    // checkForDraw();
+    hasGameEnded(board);
     changePlayer();
     board.render();
   };
@@ -50,6 +95,5 @@ const game = ((gameBoard, p1 = 'Player One', p2 = 'Player Two') => {
 })(board);
 
 (() => {
-  const grid = document.querySelector('.board');
-  grid.addEventListener('click', e => game.move(e));
+  elements.grid.addEventListener('click', e => game.move(e));
 })();
