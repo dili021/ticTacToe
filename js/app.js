cc27594c4826d514e980e6126624ac43c8038b19
stefan dili <stefan.dili.021@gmail.com>
2020-09-11 17:23:04 +0200

    extracted the game out of the event listener

--- js/app.js
+++ js/app.js
@@ -9,67 +9,69 @@ import playerFactory from './factories.js';
 import inputIsValid from './utils.js';
 
 // init game
-elements.startButton.addEventListener('click', () => {
-  const game = ((gameBoard, p1, p2) => {
-    const board = gameBoard();
-    const player1 = playerFactory(p1 || 'Player One', 'X');
-    const player2 = playerFactory(p2 || 'Player Two', 'O');
-    elements.playerOne.textContent = `${player1.name} is ${player1.mark}`;
-    elements.playerTwo.textContent = `${player2.name} is ${player2.mark}`;
-    let currentPlayer = player1;
 
-    const changePlayer = () => {
-      currentPlayer = currentPlayer === player1 ? player2 : player1;
-    };
+const game = (gameBoard, p1, p2) => {
+  const board = gameBoard();
+  const player1 = playerFactory(p1 || 'Player One', 'X');
+  const player2 = playerFactory(p2 || 'Player Two', 'O');
+  elements.playerOne.textContent = `${player1.name} is ${player1.mark}`;
+  elements.playerTwo.textContent = `${player2.name} is ${player2.mark}`;
+  let currentPlayer = player1;
+
+  const changePlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
     // check for end game
-    const checkEndGame = () => {
-      if (playerHasWon(currentPlayer) || gameIsDraw()) endGame();
-    };
+  const checkEndGame = () => {
+    if (playerHasWon(currentPlayer) || gameIsDraw()) endGame();
+  };
 
-    const endGame = () =>
-      showEndModal() && elements.grid.replayEventListener('click', game.play);
+  const endGame = () =>
+    showEndModal() && elements.grid.replayEventListener('click', game.play);
 
-    const playerHasWon = player => {
-      let res;
-      board.getWinLines().forEach(line => {
-        if (line.every(spot => board.getField()[spot] === player.mark)) {
-          elements.winner.textContent = `Congratulations ${currentPlayer.name} you won!`;
-          res = true;
-        }
-      });
-      return res;
-    };
-
-    const gameIsDraw = () => {
-      let res;
-
-      if (
-        board.getField().every(spot => spot !== null) &&
-        !playerHasWon(currentPlayer)
-      ) {
-        elements.winner.textContent = 'The game is draw. Want a rematch?';
+  const playerHasWon = player => {
+    let res;
+    board.getWinLines().forEach(line => {
+      if (line.every(spot => board.getField()[spot] === player.mark)) {
+        elements.winner.textContent = `Congratulations ${currentPlayer.name} you won!`;
         res = true;
       }
-      return res;
-    };
+    });
+    return res;
+  };
 
-    const showEndModal = () => {
-      elements.endModal.style.display = 'grid';
-    };
+  const gameIsDraw = () => {
+    let res;
 
-    const play = e => {
-      const index = e.target.dataset.cellIdx;
-      const { mark } = currentPlayer;
-      if (!inputIsValid(board, index)) return;
-      board.writeToField(index, mark);
-      checkEndGame(board);
-      changePlayer();
-      board.render();
-    };
+    if (
+      board.getField().every(spot => spot !== null) &&
+        !playerHasWon(currentPlayer)
+    ) {
+      elements.winner.textContent = 'The game is draw. Want a rematch?';
+      res = true;
+    }
+    return res;
+  };
 
-    return { play };
-  })(board, elements.playerOneInput.value, elements.playerTwoInput.value);
-  elements.grid.addEventListener('click', game.play);
+  const showEndModal = () => {
+    elements.endModal.style.display = 'grid';
+  };
+
+  const play = e => {
+    const index = e.target.dataset.cellIdx;
+    const { mark } = currentPlayer;
+    if (!inputIsValid(board, index)) return;
+    board.writeToField(index, mark);
+    checkEndGame(board);
+    changePlayer();
+    board.render();
+  };
+
+  return { play };
+};
+
+elements.startButton.addEventListener('click', () => {
+  const newGame = game(board, elements.playerOneInput.value, elements.playerTwoInput.value);
+  elements.grid.addEventListener('click', newGame.play);
 });
-
 export default board;
